ab3cc7dab7ee50916f5d4fcd855894784002ba3e
7506574455 <84278635+7506574455@users.noreply.github.com>
2021-07-06 10:41:39 -0700

    Added: binary search tree insert program

--- Main.java
+++ Main.java
@@ -2,20 +2,28 @@ package com.bs;
 
 public class Main {
 
-        //main method
 	public static void main(String[] args) {
-		//create instances
-		BinarySearchTree bst = new BinarySearchTree();
-		Node root = null;
-		//Insert node in Binary tree
 			
-		root = bst.insert(root, 56);         //  56-root node
-		root = bst.insert(root, 30);        //  30 -left node
-		root = bst.insert(root, 70);       //	70-right node	
-
-		bst.print(root);
-
+			BinarySearchTree bst = new BinarySearchTree();            //creating instances
+			Node root = null;
+			
+			
+			root = bst.insert(root, 56);                //Inserting node in the Binary tree
+			root = bst.insert(root, 30);
+			root = bst.insert(root, 70);
+			root = bst.insert(root, 22);
+			root = bst.insert(root, 40);
+			root = bst.insert(root, 60);
+			root = bst.insert(root, 95);
+			root = bst.insert(root, 11);
+			root = bst.insert(root, 65);
+			root = bst.insert(root, 3);
+			root = bst.insert(root, 16);
+			root = bst.insert(root, 63);
+			root = bst.insert(root, 67);
+			bst.print(root);
+		}
 	}
 
-}
+
 
